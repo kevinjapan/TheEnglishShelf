e0484e5c5dabd0975f4374905c41a3dd1ce1f404
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2023-10-21 11:26:32 +0100

    Ensure dark text titles for Lesson Cards

--- src/components/LessonCard/LessonCard.jsx
+++ src/components/LessonCard/LessonCard.jsx
@@ -8,12 +8,12 @@ const LessonCard = props => {
  
    let card_width = props.config['l_card_width'] !== undefined 
       ?   props.config['l_card_width'] 
-      :   "col-12 col-sm-6 col-md-6 col-lg-4"
+      :   "col-12 col-sm-6 col-md-6 col-lg-4 text-dark"
 
    return (
       <NavLink to={`/${props.service}/${props.item.slug}`}
          className={`${card_width} p-1 text-decoration-none text-dark`} key={props.item.id}>
-         <div className="m-1 lesson_card py-2">
+         <div className="m-1 lesson_card text-dark py-2">
             <div className="col-12 m-auto mt-3 pb-3 inlineLink">
                {props.list_meta.cols.map(col => (
                   col.list_col === "1" 
